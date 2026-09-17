50afcf705fdbafbfff8712b429bf37d633b09ab5
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-17 22:42:41 +0930

    make you take 0.01* the damage when driving cars.

--- lua/playermanager.lua
+++ lua/playermanager.lua
@@ -5,7 +5,8 @@ function PlayerManager:damage_reduction_skill_multiplier(...)
 
 	local player = self:player_unit()
 	if player and player:movement()._current_state_name == "driving" then
-		dmg_reduction = dmg_reduction * 0.2 -- 0.5 > 0.2, the car incident of 2024. | im actually considering setting this to 0.01, to basically remove any damage when in cars, but reduce car health to acceptable levels, forcing you to leave the car to repair it... that'd be interesting.
+		dmg_reduction = dmg_reduction * 0.01 -- 0.5 > 0.2, the car incident of 2024. | im actually considering setting this to 0.01, to basically remove any damage when in cars, but reduce car health to acceptable levels, forcing you to leave the car to repair it... that'd be interesting.
+											 -- done, although you'll have to use my complements mod to make it properly work.
 	end
 
 	return dmg_reduction
