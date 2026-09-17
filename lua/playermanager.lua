a967d7a47b5d23db12c64734fcf05455a905e1e9
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-17 20:01:26 +0930

    make driving cars not retarded. only i will remember the car incident of 2024.

--- lua/playermanager.lua
+++ lua/playermanager.lua
@@ -5,7 +5,7 @@ function PlayerManager:damage_reduction_skill_multiplier(...)
 
 	local player = self:player_unit()
 	if player and player:movement()._current_state_name == "driving" then
-		dmg_reduction = dmg_reduction * 0.5
+		dmg_reduction = dmg_reduction * 0.2 -- 0.5 > 0.2, the car incident of 2024.
 	end
 
 	return dmg_reduction
