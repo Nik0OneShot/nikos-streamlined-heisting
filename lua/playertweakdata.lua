e0b1cd4a3be68163efe1487b4c02236a83e4e34b
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-18 06:14:15 +0930

    make pro job effect grace period, always setting it to death sentence values.

--- lua/playertweakdata.lua
+++ lua/playertweakdata.lua
@@ -1,7 +1,13 @@
+local pro_job = Global.game_settings and Global.game_settings.one_down
+
 -- Give each difficulty a unique grace period time, revive health and suspicion multipliers
 -- you didn't give them grace period times.
 Hooks:PostHook(PlayerTweakData, "_set_normal", "sh__set_normal", function (self)
+	if pro_job then
+	self.damage.MIN_DAMAGE_INTERVAL = 0.1
+	else
 	self.damage.MIN_DAMAGE_INTERVAL = 0.45 -- 0.25 > 0.45
+	end
 	self.damage.REVIVE_HEALTH_STEPS = { 1, 0.85, 0.75, 0.65 } -- { 0.65 } > { 1, 0.85, 0.75, 0.65 }
 
 	self.suspicion.max_value = 6
@@ -10,7 +16,11 @@ Hooks:PostHook(PlayerTweakData, "_set_normal", "sh__set_normal", function (self)
 end)
 
 Hooks:PostHook(PlayerTweakData, "_set_hard", "sh__set_hard", function (self)
+	if pro_job then
+	self.damage.MIN_DAMAGE_INTERVAL = 0.1
+	else
 	self.damage.MIN_DAMAGE_INTERVAL = 0.35 -- 0.25 > 0.35
+	end
 	self.damage.REVIVE_HEALTH_STEPS = { 0.85, 0.75, 0.65, 0.50 } -- { 0.55 } > { 0.85, 0.75, 0.65, 0.50 }
 
 	self.suspicion.max_value = 7
@@ -19,7 +29,11 @@ Hooks:PostHook(PlayerTweakData, "_set_hard", "sh__set_hard", function (self)
 end)
 
 Hooks:PostHook(PlayerTweakData, "_set_overkill", "sh__set_overkill", function (self)
+	if pro_job then
+	self.damage.MIN_DAMAGE_INTERVAL = 0.1
+	else
 	self.damage.MIN_DAMAGE_INTERVAL = 0.3 -- 0.25 > 0.3
+	end
 	self.damage.REVIVE_HEALTH_STEPS = { 0.75, 0.65, 0.50, 0.35 } -- { 0.45 } > { 0.75, 0.65, 0.50, 0.35 }
 
 	self.suspicion.max_value = 8
@@ -28,7 +42,11 @@ Hooks:PostHook(PlayerTweakData, "_set_overkill", "sh__set_overkill", function (s
 end)
 
 Hooks:PostHook(PlayerTweakData, "_set_overkill_145", "sh__set_overkill_145", function (self)
+	if pro_job then
+	self.damage.MIN_DAMAGE_INTERVAL = 0.1
+	else
 	self.damage.MIN_DAMAGE_INTERVAL = 0.25
+	end
 	self.damage.REVIVE_HEALTH_STEPS = { 0.65, 0.50, 0.35, 0.25 } -- { 0.35 } > { 0.65, 0.50, 0.35, 0.25 }
 
 	self.suspicion.max_value = 9
@@ -37,7 +55,11 @@ Hooks:PostHook(PlayerTweakData, "_set_overkill_145", "sh__set_overkill_145", fun
 end)
 
 Hooks:PostHook(PlayerTweakData, "_set_easy_wish", "sh__set_easy_wish", function (self)
+	if pro_job then
+	self.damage.MIN_DAMAGE_INTERVAL = 0.1
+	else
 	self.damage.MIN_DAMAGE_INTERVAL = 0.2 -- 0.25 > 0.2
+	end
 	self.damage.REVIVE_HEALTH_STEPS = { 0.50, 0.35, 0.25, 0.15 } -- { 0.25 } > { 0.50, 0.35, 0.25, 0.15 }
 
 	self.suspicion.max_value = 10
@@ -46,7 +68,11 @@ Hooks:PostHook(PlayerTweakData, "_set_easy_wish", "sh__set_easy_wish", function 
 end)
 
 Hooks:PostHook(PlayerTweakData, "_set_overkill_290", "sh__set_overkill_290", function (self)
+	if pro_job then
+	self.damage.MIN_DAMAGE_INTERVAL = 0.1
+	else
 	self.damage.MIN_DAMAGE_INTERVAL = 0.15 -- 0.25 > 0.15
+	end
 	self.damage.REVIVE_HEALTH_STEPS = { 0.35, 0.25, 0.15, 0.1 } -- { 0.15 } > { 0.35, 0.25, 0.15, 0.1 }
 
 	self.suspicion.max_value = 11
@@ -55,7 +81,11 @@ Hooks:PostHook(PlayerTweakData, "_set_overkill_290", "sh__set_overkill_290", fun
 end)
 
 Hooks:PostHook(PlayerTweakData, "_set_sm_wish", "sh__set_sm_wish", function (self)
+	if pro_job then
+	self.damage.MIN_DAMAGE_INTERVAL = 0.1
+	else
 	self.damage.MIN_DAMAGE_INTERVAL = 0.1 -- 0.25 > 0.1
+	end
 	self.damage.REVIVE_HEALTH_STEPS = { 0.25, 0.15, 0.1, 0.05 } -- { 0.05 } > { 0.25, 0.15, 0.1, 0.05 }
 
 	self.suspicion.max_value = 12
