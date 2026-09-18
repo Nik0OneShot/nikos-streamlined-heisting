4bf5f69b00109af5b33ca0fbe3216c7ac42d846e
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-19 04:24:08 +0930

    formatting

--- lua/timergui.lua
+++ lua/timergui.lua
@@ -13,6 +13,7 @@ function DrillTimerGui:set_jammed(jammed)
 		self:_set_jammed(jammed)
 	end
 end
+
 -- Skip next scheduled jam if it's going to happen very shortly after unjamming
 Hooks:PostHook(TimerGui, "set_jammed", "sh_set_jammed", function (self, jammed)
 	if not jammed and self._current_jam_timer and self._current_jam_timer < 5 / self:get_timer_multiplier() then
