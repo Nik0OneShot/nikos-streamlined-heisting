3760f6e23d33913b959705b94311c07e436c5446
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-19 05:26:42 +0930

    yeah i really shouldn't be programming at 5:26 in the morning.

--- lua/playermovement.lua
+++ lua/playermovement.lua
@@ -15,10 +15,10 @@ end)
 
 -- then make cloaker kicks cuff you instead, but if on pro job, force incapacitation state. todo: make revive health be whatever health you had before being kicked, then decreased by 25% of your max HP to a minimum of 1 hp.
 
-Hooks(PostHook, "on_SPOOCed", "cuff_cloakers", function(self)
-    if self._current_state_name == "standard" or self._current_state_name == "bleed_out" or self._current_state_name == "carry" or self._current_state_name == "tased" or self._current_state_name == "bipod" or ~= pro_job then
-      managers.player:set_player_state("arrested")
-    else
-      managers.player:set_player_state("incapacitated")
-    end
+Hooks:PostHook(PlayerMovement, "on_SPOOCed", "cuff_cloakers", function(self)
+	if pro_job then
+		managers.player:set_player_state("incapacitated")
+	else
+		managers.player:set_player_state("arrested")
+	end
 end)
