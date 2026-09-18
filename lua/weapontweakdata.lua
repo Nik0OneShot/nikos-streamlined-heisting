2f2b5cf9902423c0481d6cb6b17cf0a1f83d5cd7
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-18 21:14:51 +0930

    Update weapontweakdata.lua

--- lua/weapontweakdata.lua
+++ lua/weapontweakdata.lua
@@ -39,8 +39,9 @@ Hooks:PostHook(WeaponTweakData, "init", "sh_init", function(self, tweak_data)
 	self.ump_npc.sounds.prefix = self.schakal_crew.sounds.prefix
 
 	-- give snipers much less rate of fire. they literally fire like a fully auto gun.
+	self.m14_sniper_npc.DAMAGE = 2
 	self.m14_sniper_npc.FIRE_MODE = "single"
-	self.m14_sniper_npc.single = { fire_rate 60 / 15 } -- this should hopefully make the fire rate not dogshit.
+	self.m14_sniper_npc.single = { fire_rate 60 / 5 } -- this should hopefully make the fire rate not dogshit.
 	self.m14_sniper_npc.armor_piercing = true
 
 	-- Make weapons of the same use type have the same stats (damage increase is handled by weapon presets)
