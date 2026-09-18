122c54533dfa9a4a22695d11c4433454cdbcd83b
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-18 16:45:20 +0930

    is it you thats causing me access violations?

--- lua/charactertweakdata.lua
+++ lua/charactertweakdata.lua
@@ -435,10 +435,10 @@ function CharacterTweakData:_presets(tweak_data, ...)
 	presets.gang_member_damage.REGENERATE_TIME = 2
 	presets.gang_member_damage.REGENERATE_TIME_AWAY = 2
 	presets.gang_member_damage.ARRESTED_TIME = 60 -- 60 seconds if they get arrested.
-	presets.gang_member_damage.damage.hurt_severity = presets.hurt_severities.only_light_hurt -- make them flinch if they take damage
+	--presets.gang_member_damage.damage.hurt_severity = presets.hurt_severities.only_light_hurt -- make them flinch if they take damage
 
 	-- make team a.i have faster speeds, no run start or stop delays, and always face enemy.
-	local team_ai_tweak_names = { "russian", "german", "spanish", "american", "jowi", "old_hoxton", "female_1", "dragan", "jacket", "bonnie", "sokol", "dragon", "bodhi", "jimmy", "sydney", "wild", "chico", "max", "joy", "myh", "ecp_female", "ecp_male" }
+	--[[local team_ai_tweak_names = { "russian", "german", "spanish", "american", "jowi", "old_hoxton", "female_1", "dragan", "jacket", "bonnie", "sokol", "dragon", "bodhi", "jimmy", "sydney", "wild", "chico", "max", "joy", "myh", "ecp_female", "ecp_male" }
 	for _, tweak_name in ipairs(team_ai_tweak_names) do
 		local char_tweaks = {}
 		self[tweak_name] = char_tweaks
@@ -448,7 +448,7 @@ function CharacterTweakData:_presets(tweak_data, ...)
 		char_tweaks.always_face_enemy = true
 		char_tweaks.damage.hurt_severity = presets.hurt_severities.only_light_hurt -- make them flinch if they take damage
 	end
-
+--]]
 	-- Setup surrender presets
 	presets.surrender.easy = {
 		base_chance = 0,
