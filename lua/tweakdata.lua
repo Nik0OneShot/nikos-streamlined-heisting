6d55fca756d73d18fe386b9321be43e89e37a204
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-18 16:56:12 +0930

    team a.i in tweakdata instead...

--- lua/tweakdata.lua
+++ lua/tweakdata.lua
@@ -1,1 +1,12 @@
 			tweak_data.team_ai.stop_action.distance = 9999999999
+
+-- incorporate my fuckass team a.i mod here because APPARENTLY putting it charactertweakdata now crashes your fucking game.
+for _, v in pairs(tweak_data.character) do
+	if type(v) == "table" and v.access == "teamAI1" then
+		v.no_run_start = true
+		v.no_run_stop = true
+		v.always_face_enemy = true
+		v.crouch_move = false
+		v.weapon.weapons_of_choice = { primary = "wpn_fps_ass_amcar_npc" }
+	end
+end
