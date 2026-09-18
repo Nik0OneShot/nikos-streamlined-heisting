fcbd5601c595b619eba362d5c7652f36f566a673
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-18 18:23:47 +0930

    actually change a.i now

--- lua/tweakdata.lua
+++ lua/tweakdata.lua
@@ -1,1 +1,13 @@
-			tweak_data.team_ai.stop_action.distance = 9999999999
+tweak_data.team_ai.stop_action.distance = 9999999999
+
+for _, v in pairs(tweak_data.character) do
+	if type(v) == "table" and v.access == "teamAI1" then
+		v.no_run_start = true
+		v.no_run_stop = true
+		v.always_face_enemy = true
+		v.crouch_move = false
+		v.move_speed = tweak_data.character.presets.move_speed.lightning -- hopefully this works
+		v.weapon = clone(tweak_data.character.presets.weapon.gang_member) -- this too
+		v.weapon.weapons_of_choice = { primary = "wpn_fps_ass_amcar_npc" }
+	end
+end
