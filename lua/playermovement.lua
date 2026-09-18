842b99269ff694eebf2d536119849bc618a836bc
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-19 06:02:11 +0930

    change formatting, remove unused code

--- lua/playermovement.lua
+++ lua/playermovement.lua
@@ -1,6 +1,5 @@
 -- port code from super serious shooter that makes cloaker kicks damage you for 25% of your max hp!
-
---local pro_job = Global.game_settings.one_down
+-- then make cloaker kicks cuff you instead, but if on pro job, force incapacitation state. todo: make revive health be whatever health you had before being kicked, then decreased by 25% of your max HP to a minimum of 1 hp.
 
 Hooks:PreHook(PlayerMovement, "on_SPOOCed", "on_SPOOCed_sss", function(self)
 local pro_job = Global.game_settings.one_down
@@ -20,15 +19,3 @@ local pro_job = Global.game_settings.one_down
 		managers.player:set_player_state("arrested")
 	end
 end)
-
--- then make cloaker kicks cuff you instead, but if on pro job, force incapacitation state. todo: make revive health be whatever health you had before being kicked, then decreased by 25% of your max HP to a minimum of 1 hp.
-
--- just testing something, seeing if a prehook will work instead.
---[[Hooks:PostHook(PlayerMovement, "on_SPOOCed", "cuff_cloakers", function(self)
-	if pro_job then
-		managers.player:set_player_state("incapacitated")
-	else
-		managers.player:set_player_state("arrested")
-	end
-end)
---]]
