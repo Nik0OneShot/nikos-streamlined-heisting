d4e0c1e8a992147e5d28409f27e3719f9e267958
Niko Nisacton Nikostein the Noik <239726309+Nik0OneShot@users.noreply.github.com>
2026-09-19 05:20:30 +0930

    add super serious shooter code to playerdamage.lua for what i think to be cloaker kicks damaging you through revives.

--- lua/playerdamage.lua
+++ lua/playerdamage.lua
@@ -41,3 +41,60 @@ Hooks:PreHook(PlayerDamage, "replenish", "eclipse_replenish", function(self)
 		self._lives_init = 4
 	end
 end)
+
+-- add these from super serious shooter
+
+Hooks:PreHook(PlayerDamage, "on_incapacitated", "on_incapacitated_sss", function(self)
+	if self._bleed_out then
+		return
+	end
+
+	self._pre_incap_health_ratio = self:health_ratio()
+	self._pre_incap_revive_health_i = self._revive_health_i
+end)
+
+Hooks:PostHook(PlayerDamage, "revive", "revive_sss", function(self)
+	if not self._pre_incap_health_ratio then
+		return
+	end
+
+	self:set_health(self:_max_health() * self._pre_incap_health_ratio)
+	self._revive_health_i = self._pre_incap_revive_health_i or self._revive_health_i
+
+	self._pre_incap_health_ratio = nil
+	self._pre_incap_revive_health_i = nil
+end)
+
+function PlayerDamage:damage_health(attack_data)
+	local damage_info = {
+		result = {
+			type = "hurt",
+			variant = attack_data.variant
+		}
+	}
+
+	if self._god_mode or self._invulnerable or self._mission_damage_blockers.invulnerable then
+		self:_call_listeners(damage_info)
+		return
+	elseif self:incapacitated() then
+		return
+	elseif self._unit:movement():current_state().immortal then
+		return
+	end
+
+	if attack_data.is_percentage then
+		attack_data.damage = attack_data.damage * self:_max_health()
+	end
+	attack_data.damage = managers.player:modify_value("damage_taken", attack_data.damage, attack_data)
+
+	if self._bleed_out then
+		self:_bleed_out_damage(attack_data)
+		return
+	end
+
+	self:mutator_update_attack_data(attack_data)
+	self:_check_chico_heal(attack_data)
+
+	self:_calc_health_damage(attack_data)
+	self:_call_listeners(damage_info)
+end
